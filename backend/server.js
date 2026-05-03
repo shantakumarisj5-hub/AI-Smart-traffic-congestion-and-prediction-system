f5a98d4d14e0bff4aabba64fac64454215e078ae
shantakumari SJ <shantakumarisj5@gmail.com>
2026-05-03 15:31:19 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:3000",
-    "https://ai-smart-traffic-congestion-and-pre.vercel.app"
+    "https://ai-smart-traffic-congestion-and-pre.vercel.app",
+    "https://ai-smart-traffic-congestion-and-21ae.onrender.com"
 ];
 
 app.use(cors({
